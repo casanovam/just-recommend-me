7785ce92b0e520b89625485cd12b2e9e7e43f603
Vicente Ruben Del Pino <ruben@MacBook-Pro-de-Ruben.local>
2014-01-25 14:50:43 +0000

    Iconos and Graficos tamanio modificados

--- public/javascripts/Autocomplete/jrm-autocomplete.js
+++ public/javascripts/Autocomplete/jrm-autocomplete.js
@@ -2,7 +2,13 @@ $(function(){
 	// Initializes typeahead
 	$("#SearchBox").typeahead({
         source: cities.JRMCities,
-        items: 5
+        items: 5,
+        matcher:function (item) 
+        {
+        	if (item.toLowerCase().indexOf(this.query.trim().toLowerCase()) == 0) {
+        		return true;
+        	}
+        }
     }); 
 });
 
