32d53b0d4dfce083aa6550acbb223a3636b32cb1
casanovam <casanova@gmail.com>
2014-06-07 11:40:16 +0100

    disabled search input

--- public/javascripts/search/google-maps.js
+++ public/javascripts/search/google-maps.js
@@ -1,10 +1,10 @@
 var GoogleMaps = function() {
-		
+
 	var map;
 	var DEFAULT_CITY = "dublin";
-	
+
 	function update(city, activities) {
-	  
+
 	  var cityLoc = new google.maps.LatLng(CityLoc[city].location[0], CityLoc[city].location[1])
 	  map = new google.maps.Map(document.getElementById("map"), {
 	    center: cityLoc,
@@ -13,9 +13,9 @@ var GoogleMaps = function() {
 	  });
 	  markActivities(cityLoc, activities);
 	}
-	
+
 	function init(){
-		
+
 		var cityLoc = new google.maps.LatLng(CityLoc[DEFAULT_CITY].location[0], CityLoc[DEFAULT_CITY].location[1])
 		map = new google.maps.Map(document.getElementById("map"), {
 		    center: cityLoc,
@@ -24,28 +24,28 @@ var GoogleMaps = function() {
 		  });
 		addMarker(cityLoc, DEFAULT_CITY);
 	}
-	
+
 	function markActivities(loc, activities){
 
 
-		for(i=0; i<Math.min(6,activities.length); i++) { 
-			
+		for(i=0; i<Math.min(6,activities.length); i++) {
+
 			var lat = latLng(activities[i].location[0], activities[i].location[1]);
 			addMarker(lat, activities[i].name);
 		}
 	}
-	
+
 	function addMarker(lat, title){
-		
+
 		new google.maps.Marker({
 		      position: lat,
 		      map: map,
 		      title: title
 		  });
 	}
-	
+
 	function latLng(x, y){
-		
+
 		return new google.maps.LatLng(x, y);
 	}
 
@@ -53,11 +53,12 @@ var GoogleMaps = function() {
 
 		map.setCenter( latLng(lat, lng) );
 	}
+	
 
 	return {
 		init: init,
 		update: update,
 		center: setCenter
 	};
-	
+
 }();
