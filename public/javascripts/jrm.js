447465a4e34c33954a8a28a32ddbe3e95e5aaa94
Casanova, Miguel <CasanovaM@DNB.com>
2014-11-08 13:06:41 +0000

    Added about us section

--- public/javascripts/jrm.js
+++ public/javascripts/jrm.js
@@ -4,12 +4,23 @@ var JRM = function() {
 	
 	function init(){
 		
+		registerAboutUsListeners();
 		GoogleMaps.init();
 		Search.init();
 	}
-	
+
+	function registerAboutUsListeners(){
+		$( "#michelPhoto" ).click(function() {
+  			 window.location="https://www.linkedin.com/in/miguelangelcasanova";
+		});
+		$( "#vicentePhoto" ).click(function() {
+  			 window.location="https://www.linkedin.com/in/vrdelpino"
+		});
+		
+	}
 	return {
 		init: init
 	};
+
 	
 }();
